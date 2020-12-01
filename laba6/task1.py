e427a429c3dadb3953d58591e14affdb375c593f
litlprogram <ovramets.i@donnu.edu.ua>
2020-12-01 19:29:38 +0000

    fond

--- laba6/task1.py
+++ laba6/task1.py
@@ -1,32 +1,13 @@
 #1) Знайти голоснілітери, які не містяться у рядку.
-s=("Сивочолий чернець Нестор, схилившись над столом, пише історію свого народу.\r Цей епізод, увічнений у мармурі, – красномовне свідчення того, що люди здавна\r прагнули зберегти інформацію для своїх нащадків.")
+text = "Сивочолий чернець Нестор, схилившись над столом, пише історію свого народу.\r Цей епізод, увічнений у мармурі, – красномовне свідчення того, що люди здавна\r прагнули зберегти інформацію для своїх нащадків."
 i = 0
-l = len(s)
-tab=[]
-gol=["а","о","е","у","є","ю","і","ї","я","и"]
-
-while i < l:
-  c = s[i]
-  if c == "а" or c == "A":
-    tab.append(c)
-  if c == "о" or c == "О":
-    tab.append(c)  
-  if c == "е" or c == "E":
-    tab.append(c) 
-  if c == "у" or c == "У":
-    tab.append(c)
-  if c == "є" or c == "Є":
-    tab.append(c)
-  if c == "ю" or c == "Ю":
-    tab.append(c)
-  if c == "і" or c == "І":
-    tab.append(c)
-  if c == "ї" or c == "Ї":
-    tab.append(c)
-  if c == "я" or c == "Я":
-    tab.append(c)  
-  if c == "и":
-    tab.append(c)  
-  i=i+1
-golos=gol.intersection[tab]
-print("Відповідь:",golos)  
+lenght = len(text)
+word_dict = " аоеуєюіїяи";
+response = "аоеуєюіїяи";
+while i < lenght:
+	letter = text[i];
+	if (word_dict.find(letter) > 0):
+		response = response.replace(letter, "");
+	i = i + 1;
+# golos=gol.intersection[tab]
+print(f"Відповідь: {response}"); 
